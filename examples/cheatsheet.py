d946d0ebd6f80bf7dccccd5f3b12f7f5b77565e3
tzsiga <tzsiga@gmail.com>
2020-05-29 16:43:28 +0200

    Create cheatsheet

--- examples/cheatsheet.py
+++ examples/cheatsheet.py
@@ -1,29 +1,89 @@
 # python cheatsheet
 # =================
 
-# list available methods on object
-dir(list)
+dir(list)                                   # list available methods on object
+help(list.copy)                             # display help on particular method / function
+type(13)                                    # get type name of something
+callable(obj)                               # check if obj is a function
 
-# display help on particular method / function
-help(list.copy)
+raise TypeError('Error description here')   # throw exception
 
-# get type name of something
-type(13)
+a if condition else b                       # ternary expression
+'', 0, 0.0, 0j, [], (), {}, False, None     # falsy values
 
-# check if obj is a function
-callable(obj)
+'foo bar baz'.split()                       # split string by spaces
+list('FooOOOo')                             # split string by characters
+'   foo  '.strip()                          # trim string
+'fooOOoo'.lower()                           # transformations
+'fooOOoo'.upper()
+'fooOOoo'.title()                           # sentence case
+'13'.isdigit()                              # check if string means a number
+'Foo'.startswith('F')                       # check first character
 
-# falsy values
-'', 0, 0.0, 0j, [], (), {}, False, None
+lst = ['foo  ', 'bar ', 'baz']
+len(lst)                                    # length of list
+lst.append('foo2')
+lst.pop()
+lst.copy()
+lst.reverse()
+lst.sort(reverse=True)                      # in-place!
+sorted(lst, key=lambda w: len(w))
+'foo' in lst                                # check value in list
+lst1 + lst2                                 # concat lists
 
-# throw exception
-raise TypeError('Error description here')
+tpl = ('foo',)                              # tuple with one element
+tpl2 = ('foo', 'bar')
+len(tpl2)
+(6, 7, 8) + (9, 10)                         # concat tuples
 
-# ternary expression
-a if condition else b
+words = { 'foo': 'FOO', 'bar': 'BAR' }
+words.keys()
+words.values()
+words.items()
+'key1' in dct                               # check key in collection
+'one' in dct.values()                       # check value in collection
 
-# check key in collection
-if "key1" in dct:
+range(length)
+range(start, end, step)
 
-# check value in collection
-if 'one' in dct.values():
+min()
+max()
+abs()
+
+import random
+random.random()                             # give a random number between [0, 1)
+random.uniform(start, end)                  # random number between [start, end)
+random.normalvariate(mean, deviation)       # random number by normal distr
+random.randint(start, end)                  # random int from range
+random.choice(['rock', 'paper', 'scissors'])    # random element from a list
+
+for i in lst:
+for idx, item in enumerate(lst):
+
+list(map(lambda w: len(w), lst))            # list mapping with lambda
+[len(w) for w in lst]                       # same with list comprehension
+
+list(filter(lambda w: len(w) > 3, lst))     # filter
+[w for w in lst if len(w) > 3]              # same with list comprehension
+
+from functools import reduce                # reduce
+reduce((lambda x, y: x if x < y else y), lst)
+
+a = lambda x, y = 13: x + y                 # lambda with default argument value
+def b(n, pow = 2):                          # function with named param
+    return n ** pow
+b(pow = 3, n = 3)
+
+class SampleClass:                          # class
+    def __init__(self, arr):
+        self.arr = arr
+
+    def arrLength(self):
+        return len(self.arr)
+
+import datetime
+import math
+
+import re                                   # regex
+match_obj = re.search(r'[1-9]', 'input 123')
+value = match_obj.group()
